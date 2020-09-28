47377b804cc6eebc6e42056e237d8d37f5c761ba
allenhabibovic3 <67384514+allenhabibovic3@users.noreply.github.com>
2020-09-28 18:14:43 -0500

    Create create a grouping for a data filed in a map.py

---
+++ create a grouping for a data filed in a map.py
@@ -0,0 +1,20 @@
+from itertools import groupby
+
+rows = [
+ {'address': '5412 N CLARK', 'date': '07/01/2012'},
+ {'address': '5148 N CLARK', 'date': '07/04/2012'},
+ {'address': '5800 E 58TH', 'date': '07/02/2012'},
+ {'address': '2122 N CLARK', 'date': '07/03/2012'},
+ {'address': '5645 N RAVENSWOOD', 'date': '07/02/2012'},
+ {'address': '1060 W ADDISON', 'date': '07/02/2012'},
+ {'address': '4801 N BROADWAY', 'date': '07/01/2012'},
+ {'address': '1039 W GRANVILLE', 'date': '07/04/2012'},
+]
+
+rows.sort(key=lambda k: k['date']) # sort them by date
+#group by the date field in the list of dictionary objects
+
+for date, item in groupby(rows, key=lambda k: k['date']):
+    print(date)
+    for i in item:
+        print("--->", i)
