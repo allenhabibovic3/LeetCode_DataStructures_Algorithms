682efc84997cde353becaa5a3cbf23812b414530
allenhabibovic3 <67384514+allenhabibovic3@users.noreply.github.com>
2020-11-23 19:22:47 -0600

    Create Linked List reverse list.java

---
+++ Linked List reverse list.java
@@ -0,0 +1,28 @@
+public class LinkedList {
+	public Node head;
+	class Node {
+		public int data;
+		public Node next;
+		
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public static Node reverseList(Node head) {
+		
+		Node prev = null;
+		Node curr = head;
+		Node next = null;
+		
+		while(curr != null) {
+			next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		head = prev;
+		return head;
+	}
+}
